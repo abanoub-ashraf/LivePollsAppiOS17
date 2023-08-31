5f0b3ca8183e2c477589b69d1947d43bdcbffddb
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2023-08-31 22:42:21 +0300

    initialize firebase + setup firebase emulator suite

--- LivePolls/LivePollsApp.swift
+++ LivePolls/LivePollsApp.swift
@@ -5,10 +5,29 @@
 //  Created by Abanoub Ashraf on 31/08/2023.
 //
 
+import FirebaseCore
+import FirebaseFirestore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        ///
+        /// these settings code is for configuring firebase emulator suit
+        ///
+        let settings = Firestore.firestore().settings
+        settings.host = "127.0.0.1:8080"
+        settings.cacheSettings = MemoryCacheSettings()
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+        return true
+    }
+}
 
 @main
 struct LivePollsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()

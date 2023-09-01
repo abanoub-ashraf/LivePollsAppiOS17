b1f39fe75f6178ec4ac954105046bd281db87b92
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2023-09-01 18:20:09 +0300

    create vote view for each poll

--- LivePolls/Observables/PollViewModel.swift
+++ LivePolls/Observables/PollViewModel.swift
@@ -6,13 +6,51 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+import Foundation
+import Observation
 
-struct PollViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Observable
+class PollViewModel {
+    let db = Firestore.firestore()
+    
+    let pollId: String
+    var poll: Poll? = nil
+    
+    init(pollId: String, poll: Poll? = nil) {
+        self.pollId = pollId
+        self.poll = poll
     }
-}
-
-#Preview {
-    PollViewModel()
+    
+    @MainActor
+    func listenToPoll() {
+        db.document("polls/\(pollId)")
+            .addSnapshotListener { snapshot, error in
+                guard let snapshot else { return }
+                
+                do {
+                    let poll = try snapshot.data(as: Poll.self)
+                    
+                    withAnimation {
+                        self.poll = poll
+                    }
+                } catch {
+                    print("Failed to fetch poll")
+                }
+            }
+    }
+    
+    func incrementOption(_ option: Option) {
+        guard let index = poll?.options.firstIndex(where: { $0.id == option.id }) else { return }
+        
+        db.document("polls/\(pollId)")
+            .updateData([
+                "totalCount": FieldValue.increment(Int64(1)),
+                "option\(index).count": FieldValue.increment(Int64(1)),
+                "lastUpdatedOptionId": option.id,
+                "updatedAt": FieldValue.serverTimestamp()
+            ]) { error in
+                print(error?.localizedDescription ?? "")
+            }
+    }
 }
--- LivePolls/Views/PollView.swift
+++ LivePolls/Views/PollView.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct PollView: View {
+    var vm: PollViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                VStack(alignment: .leading) {
+                    Text("Poll ID")
+                    
+                    Text(vm.pollId)
+                        .font(.caption)
+                        .textSelection(.enabled)
+                }
+                
+                HStack {
+                    Text("Updated At")
+                    
+                    Spacer()
+                    
+                    if let updatedAt = vm.poll?.updatedAt {
+                        Text(updatedAt, style: .time)
+                    }
+                }
+                
+                HStack {
+                    Text("Total Vote Count")
+                    
+                    Spacer()
+                    
+                    if let totalCount = vm.poll?.totalCount {
+                        Text(String(totalCount))
+                    }
+                }
+            }
+            
+            if let options = vm.poll?.options {
+                Section {
+                    PollChartView(options: options)
+                        .frame(height: 200)
+                        .padding(.vertical)
+                }
+                
+                Section("Vote") {
+                    ForEach(options) { option in
+                        Button {
+                            vm.incrementOption(option)
+                        } label: {
+                            HStack {
+                                Text("+1")
+                                
+                                Text(option.name)
+                                
+                                Spacer()
+                                
+                                Text(String(option.count))
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        .navigationTitle(vm.poll?.name ?? "")
+        .onAppear {
+            vm.listenToPoll()
+        }
     }
 }
 
 #Preview {
-    PollView()
+    NavigationStack {
+        PollView(
+            vm: .init(pollId: "C5C5A514-64D4-44C4-8CED-6397969CACF1")
+        )
+    }
 }
